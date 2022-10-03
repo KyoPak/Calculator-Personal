ca7be8448beada20dbcfa8d9c15859979b068d1b
HyosungP <hyosing92@gmail.com>
2022-10-04 01:39:46 +0900

    refactor: setUILabel() 메서드 name 변경 및 setDefaultOperand() 메서드로 중복코드 제거
    - setUILabel() ➡️ setUILabelText() name 변경
    - setDefaultOperand() 메서드로 중복코드 제거

--- Calculator/Calculator/Controllers/CalculatorViewController.swift
+++ Calculator/Calculator/Controllers/CalculatorViewController.swift
@@ -20,11 +20,11 @@ final class CalculatorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUILabel()
+        setUILabelText()
         clearCalculateHistory()
     }
     
-    private func setUILabel() {
+    private func setUILabelText() {
         operatorLabel.text = Constant.empty
         resultLabel.text = Constant.zero
     }
@@ -123,9 +123,8 @@ final class CalculatorViewController: UIViewController {
         operatorLabel.text = operators
         
         if currentOperator == Constant.calculate {
-            currentOperand = Constant.defaultZero
             currentOperator = operators
-            resultLabel.text = Constant.zero
+            setDefaultOperand()
             return
         }
         
@@ -201,7 +200,7 @@ final class CalculatorViewController: UIViewController {
     }
     
     @IBAction func tappedAllClear(_ sender: UIButton) {
-        setUILabel()
+        setUILabelText()
         clearCalculateHistory()
         currentOperand = Constant.defaultZero
         currentOperator = Constant.empty
@@ -209,7 +208,6 @@ final class CalculatorViewController: UIViewController {
     }
     
     @IBAction func tappedClearEntry(_ sender: UIButton) {
-        currentOperand = Constant.defaultZero
-        resultLabel.text = Constant.zero
+        setDefaultOperand()
     }
 }
